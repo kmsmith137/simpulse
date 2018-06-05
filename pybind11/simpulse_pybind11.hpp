850fd409d3d10f95c5af27199918496c227f9eec
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2018-06-05 16:59:39 -0400

    split simpulse_pybind11 into multiple source files

---
+++ pybind11/simpulse_pybind11.hpp
@@ -0,0 +1,19 @@
+#ifndef _SIMPULSE_PYBIND11_HPP
+#define _SIMPULSE_PYBIND11_HPP
+
+#include <pybind11/pybind11.h>
+
+
+namespace simpulse_pybind11 {
+#if 0
+}  // pacify emacs c-mode
+#endif
+
+
+// wrap_single_pulse.cpp
+void wrap_single_pulse(pybind11::module &m);
+
+
+}  // namespace simpulse_pybind11
+
+#endif // _SIMPULSE_PYBIND11_HPP
