b3cbb71c80f533d656cd92c53d827bd5ccea6842
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2018-06-10 17:37:55 -0400

    don't clobber ImportError in python/simpulse/__init__.py

--- python/simpulse/__init__.py
+++ python/simpulse/__init__.py
@@ -1,8 +1,4 @@
 """simpulse: simulating pulses in radio astronomy"""
 
+from .simpulse_pybind11 import *
 from . import utils
-
-try:
-    from .simpulse_pybind11 import *
-except:
-    raise ImportError("Toplevel 'simpulse' python module couldn't import the 'simpulse_pybind11' submodule.  Maybe you need 'make install' in the simpulse build directory?")
